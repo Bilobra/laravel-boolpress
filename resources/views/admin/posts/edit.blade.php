f5f3f68a8f011b892c171d79a8dc5762f49b83e1
Bilobra <bra.carrozzo@gmail.com>
2022-10-26 15:02:12 +0200

    add cover column in posts + edit env disk public

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -45,7 +45,7 @@
               <div id="category" class="invalid-feedback">
                 {{ $message }}
               </div>
-            @enderror
+            @enderror 
           </div>
 
           <div class="form-group">
