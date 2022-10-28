f5d9ae3336a08160c0ac84c9dd9c3fe67bd37bb3
Bilobra <bra.carrozzo@gmail.com>
2022-10-28 15:48:05 +0200

    add index() in Api/PostController

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -14,7 +15,10 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        $result = Post::orderBy('created_at', 'desc')->with('category','tags')->paginate(12);
+        $success = true;
+
+        return response()->json(compact('result','success'));
     }
 
     /**
